c9bde36e937653410d9500031fa0fdfd52dde468
Nazar lelyak <nazar.lelyak@gmail.com>
2015-05-07 00:28:43 +0300

    add new implementation

--- src/main/java/com/lelyak/service/MessageService.java
+++ src/main/java/com/lelyak/service/MessageService.java
@@ -1,50 +1,46 @@
 package com.lelyak.service;
 
+import com.lelyak.database.DatabaseClass;
 import com.lelyak.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService {
-    private List<Message> allMessagesList;
+
+    private Map<Long, Message> allMessages = DatabaseClass.getMessages();
 
     public MessageService() {
-        allMessagesList = new ArrayList<>();
         // hard coded DB mock
         Message m1 = new Message(1L, "Hello World", "Lelyak");
         Message m2 = new Message(2L, "Hello Jersey", "Lelyak");
-        allMessagesList.add(m1);
-        allMessagesList.add(m2);
+        allMessages.put(1L, m1);
+        allMessages.put(2L, m2);
     }
 
     public List<Message> getAllMessages() {
-        return allMessagesList;
+        return new ArrayList<>(allMessages.values());
     }
 
     public Message getMessage(long messageId) {
-        for (Message message : allMessagesList) {
-            if (message.getId() == messageId) {
-                return message;
-            }
-        }
-        return null;
+        return allMessages.get(messageId);
     }
 
     public Message addMessage(Message message) {
-        allMessagesList.add(message);
+        message.setId(allMessages.size() + 1);
+        allMessages.put(message.getId(), message);
         return message;
     }
 
     public Message updateMessage(Message message) {
-        return null;
+        if (message.getId() < 0) {
+            return null;
+        }
+        return allMessages.put(message.getId(), message);
     }
 
-    public void removeMessage(long id) {
-        for (int i = 0; i < allMessagesList.size(); i++) {
-            long currentId = allMessagesList.get(i).getId();
-            if (currentId == id) {
-                allMessagesList.remove(i);
-            }
-        }
+    public Message removeMessage(long id) {
+        return allMessages.remove(id);
     }
 }
---
+++ src/main/java/com/lelyak/service/ProfileService.java
@@ -0,0 +1,44 @@
+package com.lelyak.service;
+
+import com.lelyak.database.DatabaseClass;
+import com.lelyak.model.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ProfileService {
+
+    private Map<String, Profile> allProfiles = DatabaseClass.getProfiles();
+
+    public ProfileService() {
+        allProfiles.put("lelyak", new Profile(1L, "lelyak", "Nazar", "Lelyak"));
+        allProfiles.put("admin", new Profile(2L, "admin", "Admin", "StrongAdmin"));
+    }
+
+    public List<Profile> getAllProfiles() {
+        return new ArrayList<>(allProfiles.values());
+    }
+
+    public Profile getProfile(String profileName) {
+        return allProfiles.get(profileName);
+    }
+
+    public Profile addProfile(Profile profile) {
+        profile.setId(allProfiles.size() + 1);
+        allProfiles.put(profile.getProfileName(), profile);
+        return profile;
+    }
+
+    public Profile updateProfile(Profile profile) {
+        if (profile.getProfileName().isEmpty()) {
+            return null;
+        }
+        allProfiles.put(profile.getProfileName(), profile);
+        return profile;
+    }
+
+    public Profile removeProfile(String profileName) {
+        return allProfiles.remove(profileName);
+    }
+}
