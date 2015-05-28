b8971a54d1978f24e82c1f4f5caf903dde3bc69e
Nazar lelyak <nazar.lelyak@gmail.com>
2015-05-28 23:09:07 +0300

    add  some fix

--- src/main/java/com/lelyak/database/DatabaseClass.java
+++ src/main/java/com/lelyak/database/DatabaseClass.java
@@ -7,7 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DatabaseClass {
+
     private static Map<Long, Message> messages = new HashMap<>();
+
     private static Map<String, Profile> profiles = new HashMap<>();
 
     public static Map<Long, Message> getMessages() {
